facf009c6ae8be180ef55c061466c94e2b4f9675
Jeff <b3589481400@gmail.com>
2022-03-25 01:15:03 +0800

    [modi] jest config

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,16 @@
 module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': "@vue/vue3-jest"
+  },
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.test.js'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
 };
--- products.test.js
+++ products.test.js
@@ -1,11 +1,22 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import products from '@/views/Front/pages/Products.vue';
+import axios from 'axios'
+
 
 test('Check the result of 5 + 2', () => {
   expect(5 + 2).toBe(7)
 });
 
+
 test('props', () => {
-  const wrapper = mount(products);
+  // const wrapper = mount(products);
+  const wrapper = shallowMount(products, {
+    global: {
+      mocks: {
+        '$http': axios
+      }
+    }
+  });
+
   expect(wrapper.findAll('[categoryItem]')).toHaveLength(1)
 });
