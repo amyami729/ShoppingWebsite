9e2cd9b44da6f710002f63694d6d1d3bf9373faa
amyami729 <medical178442@gmail.com>
2022-03-25 19:06:48 +0800

    init

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,19 @@
 module.exports = {
+    moduleFileExtensions: [
+        'js',
+        'vue'
+      ],
+      transform: {
+        // "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+        // ".*\\.(vue)$": "<rootDir>/node_modules/jest-vue"
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.vue$': "@vue/vue3-jest"
+      },
+      testEnvironment: 'jsdom',
+      moduleNameMapper: {
+        '@/(.*)$': '<rootDir>/src/$1'
+      },
+      testMatch: ['**/*.test.js'],
+    //   transformIgnorePatterns: ['<rootDir>/node_modules/']
+      
 };
--- products.test.js
+++ products.test.js
@@ -1,11 +1,34 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import products from '@/views/Front/pages/Products.vue';
+import axios from 'axios';
 
 test('Check the result of 5 + 2', () => {
   expect(5 + 2).toBe(7)
 });
 
-test('props', () => {
-  const wrapper = mount(products);
-  expect(wrapper.findAll('[categoryItem]')).toHaveLength(1)
+
+test('get category length', () => {
+  const mockRoute = {
+    query: {
+      id: 1
+    }
+  }
+  const mockRouter = {
+    push: jest.fn()
+  }
+  const wrapper = shallowMount(products, {
+    global: {
+      mocks: {
+        '$http': axios,
+        '$route': mockRoute,
+        '$router': mockRouter
+      }
+    }
+  });
+  expect(wrapper.findAll('.product-category').length).toEqual(2)
 });
+
+// target: 
+// 1.Vue.use(router)是否可以正確注入$router/$route到prototype
+// 2.注入的$router/$route是否可以成功掛載到組件實例
+// 3.$router/$route是否可以正常獲取參數($route.query)和push($router.push)
