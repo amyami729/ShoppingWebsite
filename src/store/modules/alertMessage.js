6d363e0aff7f382ca5ac94bf9b4c0e2c67e6f519
Amy Chen <medical178442@gmail.com>
2022-03-28 16:25:56 +0800

    update content

--- src/store/modules/alertMessage.js
+++ src/store/modules/alertMessage.js
@@ -9,14 +9,10 @@ export default ({
       state.messages.push(payload);
     },
     messageSplice(state, payload) {
-      // splice 中文為拼接。可以藉由刪除原來有的元素 並／或加入新元素來改變一個陣列的內容
-      // arr1.splice(要插入或刪除的索引位置, 要刪除的元素數量, 要插入的元素內容)
-      // splice()本身所回傳回來的，會是我們刪除掉的元素陣列，而不是使用splice()之後原陣列的結果，如果希望得到原陣列的結果，需要指派另一個變數去接
       state.messages.splice(payload, 1);
     }
   },
   actions: {
-    // context當作this.$store
     updateMessage(context, { message, status = 'danger' }) {
       const timestamp = Math.floor(new Date() / 1000);
       context.commit('messagePush', {
